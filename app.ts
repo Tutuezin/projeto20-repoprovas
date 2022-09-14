4f39fd4983bac1fd007dfee99cd9718de9d8fd20
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-09-14 13:31:54 -0300

    build: install dependencies

--- app.ts
+++ app.ts
@@ -14,7 +14,6 @@ app.use(router);
 app.use(errorHandler);
 
 //SERVER
-app.listen(process.env.PORT ||  4000, () =>
+app.listen(process.env.PORT || 4000, () =>
   console.log(`Server listening on port ${process.env.PORT}`)
 );
-
