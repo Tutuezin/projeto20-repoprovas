a0e9af81459dbf2494a23ecd4746287a9871b9d8
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-09-18 18:51:21 -0300

    feat: test route POST /test/create, status 422

--- tests/postTests.test.ts
+++ tests/postTests.test.ts
@@ -44,4 +44,99 @@ describe("Testing route POST /create", () => {
 
     expect(result.body).toBeInstanceOf(Object);
   });
+
+  it("Should return status 422 when name information is null", async () => {
+    const body = {
+      email: "test_email@testemail.com",
+      password: "test_email@testemail.com",
+      confirmPassword: "test_email@testemail.com",
+    };
+
+    await supertest(app).post("/signup").send(body);
+
+    const newUser = {
+      email: "test_email@testemail.com",
+      password: "test_email@testemail.com",
+    };
+
+    const loggedUser = await supertest(app).post("/signin").send(newUser);
+    const token = loggedUser.text;
+
+    const test = {
+      name: "",
+      pdfUrl: "https://www.google.com",
+      categoryId: 15,
+      teacherDisciplineId: 10,
+    };
+
+    const result = await supertest(app)
+      .post(`/test/create`)
+      .set({ Authorization: `Bearer ${token}` })
+      .send(test);
+
+    expect(result.status).toEqual(422);
+  });
+
+  /* it("Should return status 422 when categoryId information is null or not found", async () => {
+    const body = {
+      email: "test_email@testemail.com",
+      password: "test_email@testemail.com",
+      confirmPassword: "test_email@testemail.com",
+    };
+
+    await supertest(app).post("/signup").send(body);
+
+    const newUser = {
+      email: "test_email@testemail.com",
+      password: "test_email@testemail.com",
+    };
+
+    const loggedUser = await supertest(app).post("/signin").send(newUser);
+    const token = loggedUser.text;
+
+    const test = {
+      name: "test_name",
+      pdfUrl: "https://www.google.com",
+      categoryId: null,
+      teacherDisciplineId: 10,
+    };
+
+    const result = await supertest(app)
+      .post(`/create`)
+      .set({ Authorization: `Bearer ${token}` })
+      .send(test);
+
+    expect(result.status).toEqual(422);
+  });
+  it("Should return status 422 when teacherDiscipline information is null or not found", async () => {
+    const body = {
+      email: "test_email@testemail.com",
+      password: "test_email@testemail.com",
+      confirmPassword: "test_email@testemail.com",
+    };
+
+    await supertest(app).post("/signup").send(body);
+
+    const newUser = {
+      email: "test_email@testemail.com",
+      password: "test_email@testemail.com",
+    };
+
+    const loggedUser = await supertest(app).post("/signin").send(newUser);
+    const token = loggedUser.text;
+
+    const test = {
+      name: "test_pdf",
+      pdfUrl: "test_email@testemail.com",
+      categoryId: 15,
+      teacherDisciplineId: null,
+    };
+
+    const result = await supertest(app)
+      .post(`/create`)
+      .set({ Authorization: `Bearer ${token}` })
+      .send(test);
+
+    expect(result.status).toEqual(422);
+  }); */
 });
